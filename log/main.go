a5b7d4615e8aa0ce87e0dd9bd0c42950fa79b239
Rémi Jouannet <remi.jouannet@outscale.com>
2020-05-08 19:57:42 +0200

    create seperate package for db and log

---
+++ log/main.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+var (
+	loggerInfo  *log.Logger
+	loggerDebug *log.Logger
+	loggerError *log.Logger
+	loggerFatal *log.Logger
+	loggerPanic *log.Logger
+	debug       bool
+)
+
+func Init(dbg bool) {
+
+	if dbg {
+		debug = true
+	}
+
+	loggerInfo = log.New(os.Stdout, "INFO:", log.Ldate|log.Ltime)
+	loggerDebug = log.New(os.Stdout, "DEBUG:", log.Ldate|log.Ltime)
+	loggerError = log.New(os.Stdout, "ERROR:", log.Ldate|log.Ltime)
+	loggerFatal = log.New(os.Stdout, "FATAL:", log.Ldate|log.Ltime)
+	loggerPanic = log.New(os.Stdout, "PANIC:", log.Ldate|log.Ltime)
+}
+
+func Info(msg string) {
+	loggerInfo.Print(msg)
+}
+
+func Debug(msg string) {
+	if debug {
+		loggerDebug.Print(msg)
+	}
+}
+func Error(msg string) {
+	loggerError.Print(msg)
+}
+
+func Fatal(msg string) {
+	loggerFatal.Fatal(msg)
+}
+
+func Panic(msg string) {
+	loggerPanic.Panic(msg)
+}
